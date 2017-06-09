31a2f15df7462aea0e6b0952176dd9e39323d1de
Jackie Tieu <jackietieu2@gmail.com>
2017-06-08 23:35:07 -0700

    complete reverse integer

---
+++ Algorithms/7.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    let max = Math.pow(2, 31),
+        str = String(x),
+        negative = x < 0 ? '-' : '',
+        flipped = str.slice(negative ? 1 : 0).split('').reverse().join('');
+
+    if (max < +flipped) {
+        return 0;
+    } else {
+        return +(negative + flipped);
+    }
+};
