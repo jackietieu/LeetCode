cba23a887d9cea0b8ddbafb67010f650c9d06550
Jackie Tieu <jackietieu2@gmail.com>
2017-06-09 20:23:03 -0700

    complete reverse int

---
+++ Algorithms/9.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x < 0) {
+        return false;
+    }
+
+    let reverse = 0,
+        original = x;
+
+    while (x) {
+        reverse =  reverse * 10 + x % 10;
+        x = parseInt(x / 10);
+    }
+
+    return reverse === original;
+};
