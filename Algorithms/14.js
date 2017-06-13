1726400cb8fff6f497b4602d5c95221927f4ea6b
Jackie Tieu <jackietieu2@gmail.com>
2017-06-12 22:28:15 -0700

    complete longest common prefix

---
+++ Algorithms/14.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    strs.sort()
+    let i = 0,
+        longestStr = strs[0] || '',
+        shortestStr = strs[strs.length - 1] || '';
+
+    for (i; i < shortestStr.length; i++) {
+        if (longestStr[i] !== shortestStr[i]) {
+            break;
+        }
+    }
+
+    return shortestStr.substr(0,i);
+};
