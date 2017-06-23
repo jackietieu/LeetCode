49fc6e29ed0c36a38a5554b5d8336558e02b8884
Jackie Tieu <jackietieu2@gmail.com>
2017-06-23 00:13:37 -0700

    complete maximum subarray

---
+++ Algorithms/53.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let i = 0,
+        max,
+        current,
+        len = nums.length;
+
+    for (i; i < len; i++) {
+        if (!current) {
+            current = nums[i];
+            if (max === undefined || max < current) {
+                max = current;
+            }
+            continue;
+        }
+
+        if (nums[i] > current) {
+            if (current > 0) {
+                current += nums[i];
+            } else {
+                current = nums[i];
+            }
+        } else {
+            current += nums[i];
+            if (max > current && current < 0) {
+                current = undefined;
+            }
+        }
+
+        if (current > max) {
+            max = current;
+        }
+    }
+
+    return max;
+};
