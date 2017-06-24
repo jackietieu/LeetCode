d7db053be14edac553c2da5b86bc165d546a84a7
Jackie Tieu <jackietieu2@gmail.com>
2017-06-23 23:49:58 -0700

    complete single number

---
+++ Algorithms/136.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(A) {
+    var length = A.length,
+        i,
+        result = 0;
+    if (length === 1) {
+        return A[0];
+    }
+    for (i = 0; i < length; i++) {
+        result = result ^ A[i];
+    }
+    return result;
+}
